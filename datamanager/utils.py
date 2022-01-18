0eacfc0150e1505ad89ebfbbbb8933e92df87821
raoulnh <raoul@pivotandco.com>
2022-01-18 14:36:01 +0100

    [datamanager] add utils

--- datamanager/utils.py
+++ datamanager/utils.py
@@ -3,9 +3,6 @@ import os
 def get_data_path(data_file):
     return os.path.join("data/", data_file)
 
-def get_static_path(static_type, static_file):
-    return os.path.join("static/", static_type, static_file)
-
-def get_asset_path(asset_file):
-    return os.path.join("assets/", asset_file)
-
+def replace_nan_by_zeros(df):
+    df = df.fillna(0)
+    return df
